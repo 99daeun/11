5b5837faacbb09ca2606bdc3dcec899f51db128c
99daeun <lucy8631@naver.com>
2023-11-18 12:50:35 +0900

    2

--- main.c
+++ main.c
@@ -3,18 +3,10 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	int i=10;
-	char c='a';
+void main(void) {
+	int i=4;
+	int*pi=&i;
+	char*pc=&i;
+	printf("%i,%i,%i\n",i,*pi,*pc);
 	
-	int*iptr=&i;
-	char*cptr=&c;
-	int*iptr2=iptr;
-	
-	printf("i:%d,iptr:%p\n",i,(void*)iptr);
-	printf("c:%d,cptr:%p\n",c,(void*)cptr);
-	printf("iptr2:%p,value:%d\n",(void*)iptr,*iptr2);
-	
-	
-	return 0;
 }
